2ec672e046423c6fa9ce7d58f6429d18cade4257
Bernat Galmés <bernat.galmes@robotbas.com>
2025-10-02 14:34:58 +0200

    Update jest moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -6,5 +6,7 @@ export default {
   moduleDirectories: ["node_modules"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
+    '^client/(.*)$': '<rootDir>/src/client/$1',
+    '^helpers/(.*)$': '<rootDir>/src/helpers/$1',
   },
 };
