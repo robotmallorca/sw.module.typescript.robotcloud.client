a7a6701cf9aa55f9b74c876adcf607596fa90da1
Bernat Galmés <bernat.galmes@robotbas.com>
2026-01-14 14:34:41 +0100

    Add global instances alerts and data get

--- src/client/projects/service-instances.ts
+++ src/client/projects/service-instances.ts
@@ -8,6 +8,8 @@ import {
   ServiceInstanceDevicesInfo,
 } from "../../../types/ServiceInstance";
 import { ServiceInstancesRequestParams } from "../../../types/request-params";
+import { ServiceDataMeasurement } from "../../../types/services";
+import { ServiceInstanceRead } from "../../../types/ServiceInstanceRead";
 
 const logger = useLogger("service-instances-client");
 
@@ -42,6 +44,22 @@ class ServiceInstancesClient {
   ) => {
     return robotcloudApi.get<Record<string, ServiceInstanceDevicesInfo>>(`projects/${prjId}/services/${service_type}/instances/${service_id}/deviceconf`);
   };
+
+  getServiceAllInstancesData(
+    prjId: string,
+    service_type: string
+  ): Promise<AxiosResponse<ServiceDataMeasurement<any>[]>> {
+    return robotcloudApi.get<ServiceDataMeasurement<any>[]>(`projects/${prjId}/services/${service_type}/data`);
+  };
+
+  getServiceAllInstancesAlerts(
+    prjId: string,
+    service_type: string
+  ): Promise<AxiosResponse<ServiceInstanceRead<any>[]>> {
+    return robotcloudApi.get<ServiceInstanceRead<any>[]>(
+      `projects/${prjId}/services/${service_type}/alerts`);
+  };
+
 }
 
 export const serviceInstancesClient = new ServiceInstancesClient();
