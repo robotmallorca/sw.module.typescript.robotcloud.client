fb236e2439023d2e098dd938abd9111be2df7d89
Eric Lachcik <eric.lachcik@robotbas.com>
2026-02-03 14:11:13 +0100

    Added Service Endpoints

--- src/client/projects/service-instances.ts
+++ src/client/projects/service-instances.ts
@@ -5,8 +5,14 @@ import robotcloudApi from "robotCloudApi";
 import {
   RobotCloudServiceInstance,
   ServiceInstanceDetails,
+  ModifyServiceInstanceDetails,
   ServiceInstanceDevicesInfo,
+  ProjectSizeInstances
 } from "../../../types/ServiceInstance";
+import {
+  RobotCloudServiceTypeDetails,
+  RobotCloudDelete
+} from "../../../types/RobotCloudClient";
 import { ServiceInstancesRequestParams } from "../../../types/request-params";
 import { ServiceDataMeasurement } from "../../../types/services";
 import { ServiceInstanceRead } from "../../../types/ServiceInstanceRead";
@@ -41,12 +47,33 @@ class ServiceInstancesClient {
   getServiceInstance = (
     prjId: string,
     service_type: string,
-    service_id: string
+    instance_id: string
   ) => {
     return robotcloudApi.get<ServiceInstanceDetails>(
-      `projects/${prjId}/services/${service_type}/instances/${service_id}`
+      `projects/${prjId}/services/${service_type}/instances/${instance_id}`
     );
   };
+
+  putServiceInstance = (
+    prjId: string,
+    service_type: string,
+    instance_id: string
+  ) => {
+    return robotcloudApi.put<ModifyServiceInstanceDetails>(
+      `projects/${prjId}/services/${service_type}/instances/${instance_id}`
+    );
+  };
+
+  deleteServiceInstance = (
+    prjId: string,
+    service_type: string,
+    instance_id: string
+  ) => {
+    return robotcloudApi.delete<RobotCloudDelete>(
+      `projects/${prjId}/services/${service_type}/instances/${instance_id}`
+    );
+  };
+
 
   getServiceInstanceDevicesInfo = (
     prjId: string,
@@ -54,6 +81,14 @@ class ServiceInstancesClient {
     service_id: string
   ) => {
     return robotcloudApi.get<Record<string, ServiceInstanceDevicesInfo>>(`projects/${prjId}/services/${service_type}/instances/${service_id}/deviceconf`);
+  };
+
+  putServiceInstanceDevicesInfo = (
+    prjId: string,
+    service_type: string,
+    service_id: string
+  ) => {
+    return robotcloudApi.put<Record<string, ServiceInstanceDevicesInfo>>(`projects/${prjId}/services/${service_type}/instances/${service_id}/deviceconf`);
   };
 
   getServiceAllInstancesData(
@@ -69,6 +104,29 @@ class ServiceInstancesClient {
   ): Promise<AxiosResponse<ServiceInstanceRead<any>[]>> {
     return robotcloudApi.get<ServiceInstanceRead<any>[]>(
       `projects/${prjId}/services/${service_type}/alert`);
+  };
+
+  getServiceLocation (
+    prjId: string,
+    locationId: string
+  ): Promise<AxiosResponse<RobotCloudServiceTypeDetails[]>> {
+    return robotcloudApi.get<RobotCloudServiceTypeDetails[]>(
+      `projects/${prjId}/locations/${locationId}/services`);
+  };
+
+  getSizeProjectInstances (
+    prjId: string
+  ): Promise<AxiosResponse<ProjectSizeInstances>> {
+    return robotcloudApi.get<ProjectSizeInstances>(
+      `size/projects/${prjId}/instances`);
+  };
+
+  getSizeProjectServicesInstances (
+    prjId: string,
+    service_type: string
+  ): Promise<AxiosResponse<ProjectSizeInstances>> {
+    return robotcloudApi.get<ProjectSizeInstances>(
+      `size/projects/${prjId}/services/${service_type}/instances`);
   };
 
 }
