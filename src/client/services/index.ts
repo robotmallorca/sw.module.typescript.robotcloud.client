4b1e6d66a996228e5d6b642c8e227c40a705f18e
Bernat Galmés <bernat.galmes@robotbas.com>
2026-02-04 11:41:56 +0100

    Export room grouping client implementation

--- src/client/services/index.ts
+++ src/client/services/index.ts
@@ -6,12 +6,15 @@ import {
 import { roomGuestStatusClient } from "./room-guest-status-1";
 import { airQualityClient } from "./air-quality-1";
 import { roomConsumesClient } from "./room-consumes-1";
+import { roomGroupingClient } from "./room-grouping-1";
 
 export {
   RoomClimeClient,
   RoomClimeAlertsKeys,
+
   roomClimeClient,
   roomGuestStatusClient,
   airQualityClient,
   roomConsumesClient,
+  roomGroupingClient,
 };
