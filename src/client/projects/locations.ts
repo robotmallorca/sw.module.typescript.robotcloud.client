662c8b734e78cc6d6e0bcdd49abac182f4876f41
Bernat Galmés <bernat.galmes@robotbas.com>
2026-02-05 12:27:17 +0100

    Add locations missing methods

--- src/client/projects/locations.ts
+++ src/client/projects/locations.ts
@@ -69,6 +69,34 @@ class LocationsClient {
       {}
     );
   };
+
+  getLocationServiceInstances = (
+    prjId: string,
+    locId: string,
+    service_type: RobotCloudServiceType,
+    params?: LocationServiceInstancesRequestParams
+  ): Promise<AxiosResponse<RobotCloudServiceInstance[]>> => {
+    return robotcloudApi.get<RobotCloudServiceInstance[]>(
+      `projects/${prjId}/locations/${locId}/services/${service_type}/instances`,
+      {
+        params,
+      }
+    );
+  };
+
+  postLocationServiceInstances = (
+    prjId: string,
+    locId: string,
+    service_type: RobotCloudServiceType,
+    params?: LocationServiceInstancesRequestParams
+  ): Promise<AxiosResponse<CreateServiceInstance[]>> => {
+    return robotcloudApi.post<CreateServiceInstance[]>(
+      `projects/${prjId}/locations/${locId}/services/${service_type}/instances`,
+      {
+        params,
+      }
+    );
+  };
 }
 
 export const locationsClient = new LocationsClient();
