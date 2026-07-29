949b37e646b86901b2edcd09d10451c72f3dd9f4
Eric Lachcik <eric.lachcik@robotbas.com>
2026-07-29 14:37:52 +0200

    Added getAll endpoint for ServiceInstanceConfigClient

--- src/client/services/generics.ts
+++ src/client/services/generics.ts
@@ -1,39 +1,59 @@
 import type { AxiosResponse } from "axios";
 
 import robotcloudApi from "robotCloudApi";
-import { ServiceDataMeasurement, ServiceInstanceConfigClient, ServiceInstanceDataClient, ServiceInstanceAlertsClient, ServiceInstanceHistoricClient, HistoricAggregateFunction } from "../../../types/services";
-import { ServiceDataRequestParams, ServiceInstanceDataRequestParams, ServiceInstanceAlertRequestParams, ServiceAlertRequestParams, ServiceInstanceHistoricParams, ServiceInstanceHistoricAggregateParams } from "../../../types/request-params";
+import {
+  ServiceDataMeasurement,
+  ServiceInstanceConfigClient,
+  ServiceInstanceDataClient,
+  ServiceInstanceAlertsClient,
+  ServiceInstanceHistoricClient,
+  HistoricAggregateFunction,
+} from "../../../types/services";
+import {
+  ServiceDataRequestParams,
+  ServiceInstanceDataRequestParams,
+  ServiceInstanceAlertRequestParams,
+  ServiceAlertRequestParams,
+  ServiceInstanceHistoricParams,
+  ServiceInstanceHistoricAggregateParams,
+} from "../../../types/request-params";
 
-
-export class GenericInstanceConfigClient<T> implements ServiceInstanceConfigClient<T> {
+export class GenericInstanceConfigClient<
+  T,
+> implements ServiceInstanceConfigClient<T> {
   private readonly serviceName: string;
 
   constructor(serviceName: string) {
     this.serviceName = serviceName;
   }
 
-  get(
-    project_id: string,
-    instance_id: string
-  ): Promise<AxiosResponse<T>> {
+  get(project_id: string, instance_id: string): Promise<AxiosResponse<T>> {
     return robotcloudApi.get<T>(
-      `/projects/${project_id}/services/${this.serviceName}/instances/${instance_id}/configuration`
+      `/projects/${project_id}/services/${this.serviceName}/instances/${instance_id}/configuration`,
     );
   }
 
   put(
     project_id: string,
     instance_id: string,
-    new_config: T
+    new_config: T,
   ): Promise<AxiosResponse<T>> {
     return robotcloudApi.put<T>(
       `/projects/${project_id}/services/${this.serviceName}/instances/${instance_id}/configuration`,
-      new_config
+      new_config,
+    );
+  }
+
+  getAll(project_id: string): Promise<AxiosResponse<T>> {
+    return robotcloudApi.get<T>(
+      `/projects/${project_id}/services/${this.serviceName}/configuration`,
     );
   }
 }
 
-export class GenericInstanceDataClient<T> implements ServiceInstanceDataClient<T> {
+export class GenericInstanceDataClient<
+  T,
+> implements ServiceInstanceDataClient<T> {
   private readonly serviceName: string;
 
   constructor(serviceName: string) {
@@ -43,7 +63,7 @@ export class GenericInstanceDataClient<T> implements ServiceInstanceDataClient<T
   get(
     prjId: string,
     instanceId: string,
-    params?: ServiceInstanceDataRequestParams
+    params?: ServiceInstanceDataRequestParams,
   ): Promise<AxiosResponse<ServiceDataMeasurement<T>>> {
     return robotcloudApi.get<ServiceDataMeasurement<T>>(
       `/projects/${prjId}/services/${this.serviceName}/instances/${instanceId}/data`,
@@ -52,13 +72,13 @@ export class GenericInstanceDataClient<T> implements ServiceInstanceDataClient<T
         headers: {
           Accept: "application/json",
         },
-      }
+      },
     );
   }
 
   getAll(
     prjId: string,
-    params?: ServiceDataRequestParams
+    params?: ServiceDataRequestParams,
   ): Promise<AxiosResponse<ServiceDataMeasurement<T>[]>> {
     return robotcloudApi.get<ServiceDataMeasurement<T>[]>(
       `/projects/${prjId}/services/${this.serviceName}/data`,
@@ -67,12 +87,14 @@ export class GenericInstanceDataClient<T> implements ServiceInstanceDataClient<T
         headers: {
           Accept: "application/json",
         },
-      }
+      },
     );
   }
 }
 
-export class GenericInstanceAlertClient<T> implements ServiceInstanceAlertsClient<T> {
+export class GenericInstanceAlertClient<
+  T,
+> implements ServiceInstanceAlertsClient<T> {
   private readonly serviceName: string;
 
   constructor(serviceName: string) {
@@ -82,7 +104,7 @@ export class GenericInstanceAlertClient<T> implements ServiceInstanceAlertsClien
   get(
     prjId: string,
     instanceId: string,
-    params?: ServiceInstanceAlertRequestParams
+    params?: ServiceInstanceAlertRequestParams,
   ): Promise<AxiosResponse<ServiceDataMeasurement<T>>> {
     return robotcloudApi.get<ServiceDataMeasurement<T>>(
       `/projects/${prjId}/services/${this.serviceName}/instances/${instanceId}/alert`,
@@ -91,13 +113,13 @@ export class GenericInstanceAlertClient<T> implements ServiceInstanceAlertsClien
         headers: {
           Accept: "application/json",
         },
-      }
+      },
     );
   }
 
   getAll(
     prjId: string,
-    params?: ServiceAlertRequestParams
+    params?: ServiceAlertRequestParams,
   ): Promise<AxiosResponse<ServiceDataMeasurement<T>[]>> {
     return robotcloudApi.get<ServiceDataMeasurement<T>[]>(
       `/projects/${prjId}/services/${this.serviceName}/alert`,
@@ -106,12 +128,14 @@ export class GenericInstanceAlertClient<T> implements ServiceInstanceAlertsClien
         headers: {
           Accept: "application/json",
         },
-      }
+      },
     );
   }
 }
 
-export class GenericInstanceHistoricClient<T> implements ServiceInstanceHistoricClient<T> {
+export class GenericInstanceHistoricClient<
+  T,
+> implements ServiceInstanceHistoricClient<T> {
   private readonly serviceName: string;
 
   constructor(serviceName: string) {
@@ -123,7 +147,7 @@ export class GenericInstanceHistoricClient<T> implements ServiceInstanceHistoric
     instanceId: string,
     startTime: Date,
     endTime: Date,
-    params: ServiceInstanceHistoricParams
+    params: ServiceInstanceHistoricParams,
   ): Promise<AxiosResponse<ServiceDataMeasurement<T>[]>> {
     return robotcloudApi.get<ServiceDataMeasurement<T>[]>(
       `/projects/${prjId}/services/${this.serviceName}/instances/${instanceId}/historic/data`,
@@ -136,7 +160,7 @@ export class GenericInstanceHistoricClient<T> implements ServiceInstanceHistoric
         headers: {
           Accept: "application/json",
         },
-      }
+      },
     );
   }
 
@@ -147,7 +171,7 @@ export class GenericInstanceHistoricClient<T> implements ServiceInstanceHistoric
     endTime: Date,
     aggFunction: HistoricAggregateFunction,
     periode: string,
-    params: ServiceInstanceHistoricAggregateParams
+    params: ServiceInstanceHistoricAggregateParams,
   ): Promise<AxiosResponse<ServiceDataMeasurement<T>[]>> {
     return robotcloudApi.get<ServiceDataMeasurement<T>[]>(
       `/projects/${prjId}/services/${this.serviceName}/instances/${instanceId}/historic/data/aggregate`,
@@ -162,7 +186,7 @@ export class GenericInstanceHistoricClient<T> implements ServiceInstanceHistoric
         headers: {
           Accept: "application/json",
         },
-      }
+      },
     );
   }
 }
